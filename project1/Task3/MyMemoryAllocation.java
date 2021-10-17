b4335d901b9a36a00eb5e0544953485512676124
edricjchai <ec73829@pace.edu>
2021-10-17 15:49:22 -0400

    MyLinkedList and MyMemoryAllocation WIP

--- project1/Task3/MyMemoryAllocation.java
+++ project1/Task3/MyMemoryAllocation.java
@@ -1,14 +1,23 @@
 public class MyMemoryAllocation extends MemoryAllocation{
 
+    private String algo;
+    private int blockSize;
+    private MyLinkedList availableBlocks;
+    private MyLinkedList usedBlocks;
+
+    //MyMemoryAllocation constructor extending MemoryAllocation
     public MyMemoryAllocation(int mem_size, String algorithm){
         super(mem_size, algorithm);
-        MyLinkedList availableBlocks = new MyLinkedList();
-        MyLinkedList usedBlocks = new MyLinkedList();
-        availableBlocks.add(mem_size);
+        availableBlocks = new MyLinkedList();
+        usedBlocks = new MyLinkedList();
+        availableBlocks.setBlock(mem_size);
     }
 
+    //alloc() puts [size call] of availableBlocks into usedBlocks
     public int alloc(int size){
-
+        if(size < availableBlocks.getSize()){
+            availableBlocks.split(size);
+        }
         return 0;
     }
 
