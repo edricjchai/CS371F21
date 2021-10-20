6ae9aaf3e8253894399e88e3b73d8020fb3b1577
edricjchai <ec73829@pace.edu>
2021-10-20 00:23:27 -0400

    Updated Task3

--- project1/Task3/MyLinkedList.java
+++ project1/Task3/MyLinkedList.java
@@ -1,71 +1,146 @@
+import com.sun.istack.internal.NotNull;
+import com.sun.istack.internal.Nullable;
+
 import java.util.Iterator;
 
 /**
- *  Application of Singly LinkedList for MemoryAllocation blocks
+ *  Application of LinkedList for MemoryAllocation blocks
  *
  */
-public class MyLinkedList implements Iterator {
-    private Node block = null;
+
+class Block implements Comparable<Block>{
+    int offset,size;
+
+    public Block(int offset, int size){
+        this.offset = offset;
+        this.size = size;
+    }
+
+    public int getOffset(){
+        return offset;
+    }
+
+    public int getSize(){
+        return size;
+    }
+
+    public boolean isAdjacent(@NotNull Block other){
+        int end = offset + size;
+        int otherEnd = other.offset + other.size;
+        return offset == otherEnd || end == other.offset;
+    }
+
+    @Override public int compareTo(@NotNull Block other){
+        return Integer.compare(this.offset,other.offset);
+    }
+
+    @Override public String toString(){
+        return String.format("(address=%d,size=%d)",offset,size);
+    }
+}
+
+public class MyLinkedList implements Iterable {
+    int address, size;
+    Node head, tail;
+
+    public MyLinkedList(){}
+
+    public @NotNull Node add(@NotNull Block block){
+        Node next = new Node(block);
+        tail.next = next;
+        return tail = next;
+    }
 
     //Node constructor
-    public class Node{
-        int size;
-        Node next;
+    public static class Node{
+        Node prev, next;
+        Block block;
+=
+        public Node(@NotNull Block block){
+            this.block = block;
+        }
 
-        Node(int value){
-            this.size = value;
-            next = null;
+        public @NotNull Node addAfter(int address, int size){
+            Node node = new Node(block);
+            node.prev = this;
+            node.next = next;
+            next.prev = node;
+            return next = node;
+        }
+
+        public void linkLeft(@Nullable Node node){
+            this.prev = node;
+            if(node != null)
+                node.prev = this;
+        }
+        public void linkRight(@Nullable Node node) {
+            this.next = node;
+            if (node != null)
+                node.next = this;
+        }
+        @Override public @NotNull String toString(){
+            return block.toString();
         }
     }
 
-    //add method that links the blocks to the list
-    public void setBlock(int size){
-        block = new Node(size);
+    @Override public String toString(){
+        String result = "";
+        for(Iterator iterator = iterator();iterator.hasNext();)
+            result += iterator.next() + ",";
+        return result.length() > 0 ? result.substring(0,result.length() - 1) : result;
+    }
+    // add method inserts a node into the current list
+    public @NotNull Node add(int address, int size){
+        Node next = new Node(address, size);
+        tail.next = next;
+        return tail = next;
     }
 
-    //remove method removes block reference off of list
-    public void remove(int removeBlock){
-        //code here
+    //remove method removes current block reference off of list
+    public @NotNull void remove(){
+
     }
 
     //searchBlock() finds a specific block size
     public Node searchBlock(int findBlock){
-        Node holder = block;
-        while(holder.size != findBlock){
-            holder = holder.next;
-        }
-        return holder;
-    }
 
-    //split method dissects the blocks to the called split size
-    public void split(int splitSize){
-        if(splitSize < block.size){
-            Node rightHalf = new Node(block.size - splitSize);
-            if(block.next.next != null)
-                rightHalf.next = block.next.next;
-            block.size = block.size - rightHalf.size; //current value is left half
-            block.next = rightHalf;
-        }
-    }
-
-    //getSize() returns blockSize
-    public int getSize(){
-        return block.size;
     }
 
     //Iterator method implementations
     //hasNext() checks if next in linkedlist is null
-    public boolean hasNext(){
-        return block.next != null;
-    }
+    @Override public @NotNull Iterator iterator(){
+        return new Iterator() {
+            Node current, next;
+            boolean started;
 
-    //next() goes to the next reference in linkedlist
-    public Node next(){
-        if(hasNext()){
-            block = block.next;
-            return block;
-        }
-        return null;
+            @Override
+            public boolean hasNext() {
+                return next != null;
+            }
+
+            @Override
+            public Object next() {
+                current = next;
+                if(hasNext())
+                    next = current.next;
+                return current;
+            }
+
+            @Override public void remove(){
+                if(current == null)
+                    throw new IllegalStateException("Next has not been called");
+                if(current.prev != null && current.next != null){
+                    current.next.prev = current.prev;
+                    current.prev.next = current.next;
+                } else if(current.prev == null){
+                    head = current.next;
+                    head.prev = null;
+                } else{
+                    current.prev.next = null;
+                    tail = current.prev;
+                }
+            }
+        };
     }
 
 
